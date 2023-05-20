6f4a96f0ece7dc33e7b482015b7f42de228bb363
Jorge Luis <jorg3luislopezc1941@hotmail.com>
2023-05-19 21:34:40 -0500

    dos

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='hero'>
+      <div className='hero__container'>
+        <div className=''>
+            <h2>Mi Abarrotes Digital</h2>
+        </div>
+        <div className='hero__login'>
+            <h2>Login</h2>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
